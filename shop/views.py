ac92d4f38073cf1c94115f2d0a9660772cfcca7f
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-06-14 18:55:44 +0300

    1406

---
+++ shop/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView,
+                                  DetailView,
+                                  ListView,
+                                  UpdateView,
+                                  DeleteView)
+
+from .forms import CategoryCreateForm, ProductCreateForm
+from .models import Category, Product
+
+
+###########################   АДМИНКА    #########################
+
+# класс для создания новой категории
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryCreateForm
+    template_name = 'admin_pages/add_category.html'
+    success_url = reverse_lazy('staff:categories')
+
+
+# класс для просмотра категорий
+class CategoryListView(ListView):
+    model = Category
+    template_name = 'admin_pages/list_category.html'
+    context_object_name = 'categories'
+
+
+# Класс для создания товара
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductCreateForm
+    template_name = 'admin_pages/add_product.html'
+    success_url = reverse_lazy('staff:products')
+
+# Класс для отображения товаров
+class ProductListView(ListView):
+    model = Product
+    template_name = 'admin_pages/list_product.html'
+    context_object_name = 'products'
+
+
+# Класс для отображения информации о товаре
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'admin_pages/detail_product.html'
+    context_object_name = 'product'
+    slug_url_kwarg = 'slug'
+
+
+class ProductsByCategoryListView(ListView):
+    model = Product
+    template_name = 'shop/index.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        context['categories'] = categories
+        return context
+
+    def get_queryset(self):
+        # если не выбрана категория товаров
+        # возвращаем все товары
+        if not self.kwargs.get('slug'):
+            return Product.objects.all()
+        # получаем название категории из URL
+        # и возвращаем товары выбранной категории
+        category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Product.objects.filter(category=category)
+
+
+class ProductDetailClientView(DetailView):
+        model = Product
+        template_name = 'shop/product_detail.html'
+        context_object_name = 'product'
+        slug_url_kwarg = 'slug'
+
