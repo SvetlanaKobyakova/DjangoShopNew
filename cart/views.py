34e40cd675b3a7c43ce3c3d16b3868645ae0a843
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-06-19 18:42:47 +0300

    1906

--- cart/views.py
+++ cart/views.py
@@ -29,4 +29,58 @@ class Cart:
     def copy(self):
         return self.cart.copy()
 
+    # метод  добавления товара как в корзину
+    def add(self, product, quantity=1, override_quantity=False):
+        # получаем id товара из ОБЪЕКТА товара
+        product_id = str(product.id)
 
+        # если товара нет в корзине
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': 0,
+                'price': str(product.price)
+            }
+
+        # если нужно перезаписать кол-во товаров
+        if override_quantity:
+            self.cart[product_id][quantity] = quantity
+        else:
+            self.cart[product_id][quantity] += quantity
+
+        self.save()
+
+    # метод удаления товара из корзины
+    def remove(self, product):
+        product_id = str(product.id)
+        if product_id in self.cart:
+            del self.cart[product_id]
+            self.save()
+
+    # метод подсчета общего количества элементов в корзине
+    def __len__(self):
+        # считаем позиции в корзине
+        # return len(self.cart)
+        # считаем общее количество товаров в корзине
+        return sum(item['quantity'] for item in self.cart.values())
+
+    # подсчет суммы товаров в корзине
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+
+    # удаление всех товаров из корзины
+    def clear(self):
+        self.cart.clear()
+        self.save()
+
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
